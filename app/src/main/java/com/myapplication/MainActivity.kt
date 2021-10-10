f16edca561a0629a1c86e0bf9c57b5049290cb0c
omarhatem1995 <omar.hatem1230@gmail.com>
2021-10-10 05:15:04 +0200

    Initial commit

---
+++ app/src/main/java/com/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.myapplication
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    var firstFragment = FirstFragment()
+    var secondFragment = SecondFragment()
+    var thirdFragment = ThirdFragment()
+    var moreFragment = MoreFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(firstFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> replaceFragment(firstFragment)
+                R.id.quran -> replaceFragment(secondFragment)
+                R.id.azkar -> replaceFragment(thirdFragment)
+                R.id.more -> replaceFragment(moreFragment)
+            }
+            true
+        }
+
+    }
+
+
+    private fun replaceFragment(fragment:Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.flFragment , fragment)
+            transaction.commit()
+        }
+    }
+
+
+}
