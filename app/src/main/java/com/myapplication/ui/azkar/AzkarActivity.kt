f4022fe634bd98aa3e1eca865fa37fb364dad227
Omar Hatem <omar.hatem1230@gmail.com>
2022-01-05 18:11:31 +0200

    refactored AzkarActivity.kt

--- app/src/main/java/com/myapplication/ui/azkar/AzkarActivity.kt
+++ app/src/main/java/com/myapplication/ui/azkar/AzkarActivity.kt
@@ -1,25 +1,19 @@
 package com.myapplication.ui.azkar
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.myapplication.R
 import org.json.JSONArray
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.myapplication.MainActivity
-import com.myapplication.QiblahActivity
 import com.myapplication.common.Constants
 import com.myapplication.data.entities.model.AzkarModel
 import com.myapplication.databinding.ActivityZekrBinding
 import java.io.*
-import java.lang.StringBuilder
-import org.json.JSONObject
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -28,14 +22,12 @@ import kotlin.collections.ArrayList
 
 class AzkarActivity : AppCompatActivity() {
 
-    lateinit var recyclerViewAzkar: RecyclerView
-    lateinit var zekrCategory: TextView
     lateinit var binding: ActivityZekrBinding
     private val viewModel: AzkarViewModel by viewModels()
     lateinit var recyclerViewState: Parcelable
     lateinit var localTime: String
-    val cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"))
-    val updateAzkarModel = arrayListOf<AzkarModel>()
+    private val cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1:00"))
+    private val updateAzkarModel = arrayListOf<AzkarModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,40 +35,27 @@ class AzkarActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_zekr)
         binding.azkar = viewModel
         binding.lifecycleOwner = this
-        recyclerViewAzkar = findViewById<RecyclerView>(R.id.azkar_recyclerView_List)
-        zekrCategory = findViewById<TextView>(R.id.azkarCategory)
         recyclerViewState = binding.azkarRecyclerViewList.layoutManager?.onSaveInstanceState()!!
-        loadJSONFromAsset()
+        initAzkar()
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        if(isTaskRoot) {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        }
-    }
-
-    fun loadJSONFromAsset() {
+    fun initAzkar() {
         val georgianDateFormatForInsertion: DateFormat =
             SimpleDateFormat("dd-MM-yyyy", Locale("en"))
         localTime = georgianDateFormatForInsertion.format(cal.time)
         val dateSaved = viewModel.preference.getDate()
         val name = intent.getStringExtra("category")
-        zekrCategory.text = name
-        if (dateSaved == null || dateSaved?.equals(localTime) != true) {
+        binding.azkarCategory.text = name
+        if (dateSaved == null || !dateSaved?.equals(localTime)) {
             viewModel.preference.setDate(localTime)
             var json: String? = null
-            var json2: String? = null
             try {
                 val inputStream: InputStream = assets.open("azkar.json")
                 json = inputStream.bufferedReader().use { it.readText() }
                 var jsonArray = JSONArray(json)
                 val azkar: MutableList<AzkarModel> = ArrayList()
                 val allAzkar: MutableList<AzkarModel> = ArrayList()
-                for (i in 0..jsonArray.length() - 1) {
+                for (i in 0 until jsonArray.length()) {
 
                     var jsonObj = jsonArray.getJSONObject(i)
                     val zekrCount = jsonObj.getString("count")
@@ -134,7 +113,7 @@ class AzkarActivity : AppCompatActivity() {
                             binding.azkarRecyclerViewList.adapter = adapter
                             binding.azkarRecyclerViewList.adapter?.stateRestorationPolicy =
                                 RecyclerView.Adapter.StateRestorationPolicy.ALLOW
-                            binding.azkarRecyclerViewList.getLayoutManager()
+                            binding.azkarRecyclerViewList.layoutManager
                                 ?.onRestoreInstanceState(recyclerViewState)
                     }
 
@@ -142,6 +121,16 @@ class AzkarActivity : AppCompatActivity() {
 
             }
 
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(isTaskRoot) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 
