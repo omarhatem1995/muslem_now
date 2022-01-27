4cc25ce5633e94721b2d6e882ad5b52cfbf45c9a
Omar Hatem <omar.hatem1230@gmail.com>
2022-01-27 19:47:04 +0200

    added QuranActivity.kt

---
+++ app/src/main/java/com/myapplication/ui/fragments/quran/QuranActivity.kt
@@ -0,0 +1,12 @@
+package com.myapplication.ui.fragments.quran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myapplication.R
+
+class QuranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quran)
+    }
+}
